ee035da1582ecf213815fae342e92ef854eb598a
Timothy Heider <timothy.heider@gmail.com>
2022-05-06 10:30:15 -0700

    load all data

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -8,36 +8,43 @@ angular.module("AprendizApplication").service('ClassroomDataLoaderService', func
             method: 'GET',
             url: 'webresources/classroom/courses'
         }).then(
-                function (response) {
-                    let courses = response.data;
-                    angular.forEach(courses, function (course) {
-                        $http({
-                            method: 'GET',
-                            url: 'webresources/classroom/topics/' + course.id
-                        }).then(function (response) {
-                            if (response.data) {
-                                course.topics = response.data;
-                                $http({
-                                    method: 'GET',
-                                    url: 'webresources/classroom/coursework/' + course.id
-                                }).then(function (response) {
-                                    if (response.data) {
-                                        course.courseWork = response.data;
-                                        $http({
-                                            method: 'GET',
-                                            url: 'webresources/classroom/courseworkmaterials/' + course.id
-                                        }).then(function (response) {
-                                            if (response.data) {
-                                                course.courseWorkMaterials = response.data;
+            function (response) {
+                let courses = response.data;
+                let courseCount = courses.length;
+                console.log('load ' + courseCount + ' courses');
+                angular.forEach(courses, function (course) {
+                    $http({
+                        method: 'GET',
+                        url: 'webresources/classroom/topics/' + course.id
+                    }).then(function (response) {
+                        if (response.data) {
+                            course.topics = response.data;
+                            $http({
+                                method: 'GET',
+                                url: 'webresources/classroom/coursework/' + course.id
+                            }).then(function (response) {
+                                if (response.data) {
+                                    course.courseWork = response.data;
+                                    $http({
+                                        method: 'GET',
+                                        url: 'webresources/classroom/courseworkmaterials/' + course.id
+                                    }).then(function (response) {
+                                        if (response.data) {
+                                            course.courseWorkMaterials = response.data;
+                                            courseCount--;
+                                            if(courseCount === 0) {
+                                                console.log('all courses loaded');
+                                                completionCallback(courses);
                                             }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-
+                                        }
+                                    });
+                                }
+                            });
+                        }
                     });
+
                 });
+            });
     };
 });
 
@@ -55,54 +62,39 @@ class MainScene extends Phaser.Scene {
 
 };
 
-class BootScene extends Phaser.Scene {
-
-    dataLoadedCallback(courses) {
-        console.log('data loaded callback', courses);
-        this.scene.add('MainScene', MainScene, true, courses);
-    }
-
-    create() {
-        console.log('created BootScene');
-        // load all the data
-        let $scope = this.config.scope;
-        $scope.loadAllData(dataLoadedCallback);
-    }
-
-};
-
 angular.module("AprendizApplication").controller('MainViewController', function ($scope, $http, ClassroomDataLoaderService) {
     console.log('started main view controller');
-
-    let config = {
-        type: Phaser.AUTO,
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: {y: 200}
-            }
-        },
-        dom: {
-            createContainer: true
-        },
-        fps: {
-            target: 16,
-            forceSetTimeOut: true
-        },
-        parent: 'aprendiz-block',
-        width: 1920,
-        height: 1080,
-        scene: BootScene,
-        scope: $scope
+    
+    $scope.dataLoaded = function(courses) {
+        console.log('data loaded', courses);
+        let config = {
+            type: Phaser.AUTO,
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: {y: 200}
+                }
+            },
+            dom: {
+                createContainer: true
+            },
+            fps: {
+                target: 16,
+                forceSetTimeOut: true
+            },
+            parent: 'aprendiz-block',
+            width: 1920,
+            height: 1080,
+            scene: MainScene,
+            courseData: courses
+        };        
+        $scope.game = new Phaser.Game(config);
     };
 
+    ClassroomDataLoaderService.loadData($scope.dataLoaded);
 
-    $scope.game = new Phaser.Game(config);
     
-    $scope.loadAllData = function(completionCallback) {
-        console.log('load all data');
-    };
-    
+        
 });
 
 
