fb960135335e5e61d99f781b5a059b0e7dc6f2fc
Timothy Heider <timothy.heider@gmail.com>
2022-05-06 08:32:14 -0700

    start main scene

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,6 +2,19 @@
 
 angular.module("AprendizApplication", []);
 
+class MainScene extends Phaser.Scene {
+
+    preload() {
+        this.load.setBaseURL('https://aprendiz-dashboard.pptpdx.com');
+        console.log('preloaded MainScene');
+    }
+
+    create () {
+        console.log('created MainScene');
+    }    
+
+};
+
 angular.module("AprendizApplication").controller('MainViewController', function ($scope, $http) {
     console.log('started main view controller');
     $http({
@@ -36,7 +49,7 @@ angular.module("AprendizApplication").controller('MainViewController', function 
                         }
                     });
                 });
-            });
+            });                       
 
     var config = {
         type: Phaser.AUTO,
@@ -56,20 +69,10 @@ angular.module("AprendizApplication").controller('MainViewController', function 
         parent: 'aprendiz-block',
         width: 1920,
         height: 1080,
-        scene: {
-            preload: preload,
-            create: create
-        }
+        scene: MainScene
     };
 
-    var game = new Phaser.Game(config);
-
-    function preload() {
-        this.load.setBaseURL('https://aprendiz-dashboard.pptpdx.com');
-    }
-
-    function create() {
-        console.log('created Aprendiz dashboard game');
-    }
+    $scope.game = new Phaser.Game(config);
+    console.log('started game controller');
 
 });
