cd60b2284e5e2b21d37c184ad98721e21e07a08c
Timothy Heider <timothy.heider@gmail.com>
2022-05-06 17:25:31 -0700

    style

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -59,20 +59,20 @@ class MainScene extends Phaser.Scene {
         console.log('created MainScene');
         // now we have access to courses
         let courses = this.game.config.courses;
-        let y = 100;
+        let y = 32;
         for(const course of courses) {
             
             let graphics = this.add.graphics();
-            graphics.lineStyle(2, 0xffff00, 1);
+            graphics.lineStyle(2, 0xffff00, 2);
 
             //  32px radius on the corners
-            graphics.strokeRoundedRect(32, 32 + y, 300, 90, 32);
+            graphics.strokeRoundedRect(32, 32 + y, 500, 80, 32);
 
             graphics.lineStyle(4, 0xff00ff, 1);
 
             this.add.text(48, 48 + y, course.name, { fontSize: '32px' });
 
-            y += 100;
+            y += 96;
         }
     }
 
