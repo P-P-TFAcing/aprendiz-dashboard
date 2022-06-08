bfce0905fb96dacfabecdc42e6a5620e544079c0
Timothy Heider <timothy.heider@gmail.com>
2022-06-08 08:28:08 -0700

    use latest data

--- src/main/webapp/js/MainScene.js
+++ src/main/webapp/js/MainScene.js
@@ -1,3 +1,50 @@
+class CourseRect {
+    constructor(scene, course, x, y) {
+        let graphics = scene.add.graphics();
+        graphics.lineStyle(2, 0xffff00, 2);
+        this.objectHeight = 50;
+        this.objectWidth = 400;        
+        //  32px radius on the corners
+        graphics.strokeRoundedRect(32 + x, 32 + y, this.objectWidth, this.objectHeight, 32);
+        graphics.lineStyle(4, 0xff00ff, 2);
+        scene.add.text(48 + x, 48 + y, course.name, { fontSize: '28px' });    
+    }
+}
+
+class LegendTopicRect {
+    constructor(scene, course, topic, x, y) {
+        let graphics = scene.add.graphics();
+        graphics.lineStyle(2, 0xffffff, 2);
+        //  32px radius on the corners
+        graphics.strokeRoundedRect(32 + x, 32 + y, 400, 50, 32);
+        graphics.lineStyle(4, 0xff00ff, 1);
+        scene.add.text(48 + x, 48 + y, topic.name, { fontSize: '24px' });  
+        this.objectHeight = 50;
+        this.objectWidth = 400;
+    }
+}
+
+class LegendRect {
+    constructor(scene, course, x, y) {
+        let ypos = y;
+        let width = undefined;
+        let height = undefined;
+        for(const topic of course.topics) {
+            let legendTopicRect = new LegendTopicRect(scene, course, topic, x, ypos);
+            ypos += (legendTopicRect.objectHeight + 4);            
+            width = legendTopicRect.objectWidth;
+            height += (legendTopicRect.objectHeight + 4);
+        }
+        this.objectHeight = width;
+        this.objectWidth = height;        
+        let graphics = scene.add.graphics();
+        graphics.lineStyle(2, 0xffffff, 2);
+        //  32px radius on the corners
+        graphics.strokeRoundedRect(16 + x, 16 + y, this.objectWidth, this.objectHeight, 16);
+        graphics.lineStyle(4, 0xff00ff, 1);
+    }
+}
+
 class MainScene extends Phaser.Scene {
 
     preload() {
@@ -9,21 +56,10 @@ class MainScene extends Phaser.Scene {
         console.log('created MainScene');
         // now we have access to courses
         let courses = this.game.config.courses;
-        let y = 32;
-        for(const course of courses) {
-            
-            let graphics = this.add.graphics();
-            graphics.lineStyle(2, 0xffff00, 2);
-
-            //  32px radius on the corners
-            graphics.strokeRoundedRect(32, 32 + y, 500, 80, 32);
-
-            graphics.lineStyle(4, 0xff00ff, 1);
-
-            this.add.text(48, 48 + y, course.name, { fontSize: '32px' });
-
-            y += 96;
-        }
+        let course = courses[0];
+        console.log('loading course',course);
+        let courseRect = new CourseRect(this, course, 32, 32);
+        new LegendRect(this, course, courseRect.objectWidth + 32, 32);
     }
 
 };
