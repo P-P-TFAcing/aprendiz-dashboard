3dd404ec2423537f378e6ccf7ad4e6cb2ed3efc7
Timothy Heider <timothy.heider@gmail.com>
2022-06-11 16:17:00 -0700

    legend text spacing

--- src/main/webapp/js/MainScene.js
+++ src/main/webapp/js/MainScene.js
@@ -59,7 +59,7 @@ class LegendTopicRect {
         legendRoundedRect.setStrokeStyle(2, 0xffffff, 2);
         container.add(legendRoundedRect);
         //  32px radius on the corners        
-        let legendTitleText = scene.add.text(x, y, topic.name, {fontSize: '24px'});
+        let legendTitleText = scene.add.text(x + 16, y + 16, topic.name, {fontSize: '24px'});
         container.add(legendTitleText);
         container.add(legendRoundedRect);
         this.objectHeight = legendRoundedRect.height;
