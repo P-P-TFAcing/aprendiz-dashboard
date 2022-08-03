1d1d60d5bc96f57457b7259eb6f03476ad4d3fd3
Timothy Heider <timothy.heider@gmail.com>
2022-08-03 10:29:56 -0700

    more webpack fun

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src/main/js/app.js'),
   output: {
       path: path.resolve(__dirname, 'src/main/webapp'),
       filename: 'bundle.js'
-  },
-  devtool: 'inline-source-map'
+  }  
 };
 
