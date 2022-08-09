b8436c72d7d08a01eac3cfcdc9a150d0e9cbceb4
Timothy Heider <timothy.heider@gmail.com>
2022-08-09 14:47:50 -0700

    websocket

--- src/main/js/WebSocketContext.js
+++ src/main/js/WebSocketContext.js
@@ -13,6 +13,7 @@ export default class WebSocketContext {
             console.log('send message',messageType, message);
         }
         this.webSocket.send(JSON.stringify(message));
+        
     }
 
     pingPeriodic() {
