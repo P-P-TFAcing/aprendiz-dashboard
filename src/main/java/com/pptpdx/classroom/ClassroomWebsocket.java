9cfe589c2f0bd89259717622e3b21ea81ac5f963
Timothy Heider <timothy.heider@gmail.com>
2022-08-10 10:16:24 -0700

    save data progress

--- src/main/java/com/pptpdx/classroom/ClassroomWebsocket.java
+++ src/main/java/com/pptpdx/classroom/ClassroomWebsocket.java
@@ -1,5 +1,7 @@
 package com.pptpdx.classroom;
 
+import com.google.gson.Gson;
+import java.util.Map;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -29,7 +31,11 @@ public class ClassroomWebsocket {
     
     @OnMessage
     public void onMessage(Session session, String message) {        
-        LOGGER.debug("websocket message received " + message);        
+        LOGGER.debug("websocket message received " + message);    
+        Gson gson = new Gson();
+        //{"containerPositions":{"CourseWorkRect_487896080085":{"x":685,"y":269},"CourseWorkRect_487896080137":{"x":641,"y":146},"LegendRect":{"x":1091,"y":33}},"messageType":"SAVE_COURSE_CONFIGURATION"}
+        Map<String, Object> data = gson.fromJson(message, Map.class);
+        LOGGER.debug("received websocket data " + data);
     }
     
     @OnClose
