da39e4aed8e2ccf54ac6f0a26abee197bcea3f31
Timothy Heider <timothy.heider@gmail.com>
2022-08-10 11:00:55 -0700

    course configuration

--- src/main/java/com/pptpdx/model/CourseConfiguration.java
+++ src/main/java/com/pptpdx/model/CourseConfiguration.java
@@ -16,6 +16,19 @@ public class CourseConfiguration implements Serializable {
     @Column(name = "id")
     private Long id;
 
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String configurationText;
+    
+    private Long courseId;
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+    
     public Long getId() {
         return id;
     }
@@ -23,9 +36,6 @@ public class CourseConfiguration implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-    
-    @Column(columnDefinition="MEDIUMTEXT")
-    private String configurationText;    
 
     public String getConfigurationText() {
         return configurationText;
@@ -34,11 +44,10 @@ public class CourseConfiguration implements Serializable {
     public void setConfigurationText(String configurationText) {
         this.configurationText = configurationText;
     }
-    
+
     @Override
     public String toString() {
         return "CourseConfiguration{" + "id=" + id + '}';
     }
-    
-            
+
 }
