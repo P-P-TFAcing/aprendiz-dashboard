1e98822a312af40e9e8277a64c499b8ed3246f00
Timothy Heider <timothy.heider@gmail.com>
2022-09-09 18:15:32 -0700

    big canvas

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -279,8 +279,8 @@ angular.module("AprendizApplication").controller('MainViewController', function 
     let config = {
         type: Phaser.AUTO,
         parent: 'aprendiz-block',
-        width: 1920,
-        height: 1080
+        width: 5120,
+        height: 2880
     };
 
     let game = new Phaser.Game(config);
