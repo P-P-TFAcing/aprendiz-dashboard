4f7799911fc3ab71c1cf901cc9b1db83e666b7c6
Timothy Heider <timothy.heider@gmail.com>
2022-09-10 09:37:00 -0700

    button

--- src/main/js/Button.js
+++ src/main/js/Button.js
@@ -6,21 +6,20 @@ export default class Button {
     constructor(scene, x, y, caption) {
         this.scene = scene;
         this.container = scene.add.container(x, y);
-        this.container.depth = 2;
         this.x = x;
         this.y = y;
+        let rectangle = scene.add.rectangle(0, 0, 32, 32);
         let text = scene.add.text(8, 8, caption, {font: "20px Arial", color: '#000000'});
         text.setOrigin(0, 0);
         text.depth = 1;
-        this.container.add(text);
         this.width = text.width + 16;
-        this.height = text.height + 16;
-        let rectangle = scene.add.rectangle(0, 0, this.width, this.height);
-        rectangle.setOrigin(0, 0);
-        rectangle.depth = 0;
+        this.height = text.height + 16;        
+        rectangle.setOrigin(0, 0);        
         rectangle.setStrokeStyle(2, 0xEEEEEE, 2);
         rectangle.setFillStyle(0xAAAAAA);
+        rectangle.setSize(this.width, this.height);
         this.container.add(rectangle);
+        this.container.add(text);
 
         rectangle.setInteractive({useHandCursor: true}).on('pointerdown', this.mouseDownHandler.bind(this));
         rectangle.setInteractive({useHandCursor: true}).on('pointerup', this.mouseUpHandler.bind(this));
