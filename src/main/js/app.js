c308bb98830473c6da6ff356cfdd0e6c66d3ebd0
Timothy Heider <timothy.heider@gmail.com>
2022-09-09 18:17:23 -0700

    double HD

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -279,8 +279,8 @@ angular.module("AprendizApplication").controller('MainViewController', function 
     let config = {
         type: Phaser.AUTO,
         parent: 'aprendiz-block',
-        width: 5120,
-        height: 2880
+        width: 3840,
+        height: 2160
     };
 
     let game = new Phaser.Game(config);
