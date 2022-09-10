e0a1dc93c67f6c01ae55f255ad2662f1530f474b
Timothy Heider <timothy.heider@gmail.com>
2022-09-10 08:46:38 -0700

    button panel

--- src/main/js/ButtonPanel.js
+++ src/main/js/ButtonPanel.js
@@ -3,7 +3,7 @@
  */
 export default class ButtonPanel {
 
-    constructor(scene, x, y, title) {
+    constructor(scene, x, y, title) {        
         this.scene = scene;
         this.container = scene.add.container(x, y);
         this.x = x;
@@ -11,11 +11,13 @@ export default class ButtonPanel {
         let text = scene.add.text(16, 16, title, {fontSize: '2em'});
         text.setOrigin(0, 0);
         this.container.add(text);
-        this.width = text.width + 32;
+        this.width = text.width + 128;
         this.height = text.height + 32;
+        this.buttonLeftPos = text.width + 32;
         let panelRectangle = scene.add.rectangle(0, 0, this.width, this.height);
         panelRectangle.setOrigin(0, 0);
         panelRectangle.setStrokeStyle(2, 0xffffff, 2);
+        panelRectangle.setFillStyle(0x333333, 0.2);        
         this.container.add(panelRectangle);
         this.panelRectangle = panelRectangle;
     }
