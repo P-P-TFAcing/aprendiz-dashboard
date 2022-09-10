0fa0e807db1c006514f4cf4ec5f88e582fad53d6
Timothy Heider <timothy.heider@gmail.com>
2022-09-10 09:24:39 -0700

    button

--- src/main/js/Button.js
+++ src/main/js/Button.js
@@ -8,37 +8,40 @@ export default class Button {
         this.container = scene.add.container(x, y);
         this.x = x;
         this.y = y;
-        let text = scene.add.text(8, 8, caption, { font: "20px Arial", color: '#000000' });
-        text.setOrigin(0, 0);
-        this.container.add(text);
-        this.width = text.width + 16;
-        this.height = text.height + 16;
-        let rectangle = scene.add.rectangle(0, 0, this.width, this.height);
+        let rectangle = scene.add.rectangle(0, 0, 32, 32);
         rectangle.setOrigin(0, 0);
         rectangle.setStrokeStyle(2, 0xEEEEEE, 2);
         rectangle.setFillStyle(0xAAAAAA);
         this.container.add(rectangle);
+        let text = scene.add.text(8, 8, caption, {font: "20px Arial", color: '#000000'});
+        text.setOrigin(0, 0);
+        this.container.add(text);
+        this.width = text.width + 16;
+        this.height = text.height + 16;
+        rectangle.width = this.width;
+        rectangle.height = this.height;
+
         rectangle.setInteractive({useHandCursor: true}).on('pointerdown', this.mouseDownHandler.bind(this));
         rectangle.setInteractive({useHandCursor: true}).on('pointerup', this.mouseUpHandler.bind(this));
         this.buttonRectangle = rectangle;
     }
-    
+
     setPosition(x, y) {
         this.container.setPosition(x, y);
     }
-    
+
     onButtonClick() {
         // possible override
         this.scene.data.buttonClicked = true;
     }
-    
+
     mouseUpHandler(event) {
         this.buttonRectangle.setFillStyle(0xAAAAAA, 0.8);
     }
 
     mouseDownHandler(event) {
         console.log('button click');
-        if(this.onButtonClick) {
+        if (this.onButtonClick) {
             this.buttonRectangle.setFillStyle(0xDDDDDD, 0.8);
             this.onButtonClick();
         }
