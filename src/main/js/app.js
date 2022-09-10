9da2b60576db964d0e703af36711b165b0ae8a7d
Timothy Heider <timothy.heider@gmail.com>
2022-09-10 08:23:33 -0700

    dynamic canvas size

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -284,8 +284,8 @@ angular.module("AprendizApplication").controller('MainViewController', function 
     let config = {
         type: Phaser.AUTO,
         parent: 'aprendiz-block',
-        width: 1920,
-        height: 1080
+        width: screenWidth,
+        height: screenHeight
     };
 
     let game = new Phaser.Game(config);
