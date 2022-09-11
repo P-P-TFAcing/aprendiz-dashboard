d75ad12a3080905a79c9dae88441f287cb188f39
Timothy Heider <timothy.heider@gmail.com>
2022-09-11 06:37:24 -0700

    scroll

--- src/main/js/ScrollableContainer.js
+++ src/main/js/ScrollableContainer.js
@@ -3,12 +3,14 @@ export default class ScrollableContainer extends Phaser.GameObjects.Container {
     constructor(scene, x, y) {
         super(scene, x, y);
     }
+    
+    updateChildOffset(gameObject) {
+        console.log('update object', gameObject);
+    }
 
     setOffset(x, y) {
         console.log('scrollable set offset', x, y);
-        for(const gameObject in this.list) {
-            console.log('update object', gameObject);
-        }
+        this.iterate(updateChildOffset);
     }
 
 }
