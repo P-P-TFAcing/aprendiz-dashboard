64607f8b39979ff19da790862f1afad37b43d30c
Timothy Heider <timothy.heider@gmail.com>
2022-09-11 06:42:24 -0700

    update offset

--- src/main/js/ScrollableContainer.js
+++ src/main/js/ScrollableContainer.js
@@ -2,14 +2,18 @@ export default class ScrollableContainer extends Phaser.GameObjects.Container {
 
     constructor(scene, x, y) {
         super(scene, x, y);
+        this.sceneOffset = { x:0, y:0 };
     }
     
     updateChildOffset(gameObject) {
         console.log('update object', gameObject);
+        gameObject.setOffset(this.sceneOffset.x, this.sceneOffset.y)
     }
 
     setOffset(x, y) {
         console.log('scrollable set offset', x, y);
+        this.sceneOffset.x = x;
+        this.sceneOffset.y = y;
         this.iterate(this.updateChildOffset);
     }
 
