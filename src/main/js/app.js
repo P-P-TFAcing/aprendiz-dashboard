2901f82a521926d713dfdf2cc2f9c6722c7fa4d2
Timothy Heider <timothy.heider@gmail.com>
2022-09-11 05:25:48 -0700

    desktop scrolling work

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -28,7 +28,7 @@ class LoaderScene extends Phaser.Scene {
         this.progressTotalCount = count;
         this.progressBar.clear();
         this.progressBar.fillStyle(0xffffff, 1);
-        this.progressBar.fillRect(50, 100, 100 * position, 50);
+        this.progressBar.fillRect(50, 100, 800 * (position / count), 50);
     }
 
     create() {
@@ -38,7 +38,7 @@ class LoaderScene extends Phaser.Scene {
         this.progressBar = this.add.graphics();
         this.progressBox = this.add.graphics();
         this.progressBox.fillStyle(0x888888, 0.8);
-        this.progressBox.fillRect(50, 100, 500, 50);
+        this.progressBox.fillRect(50, 100, 800, 50);
     }
 }
 ;
@@ -78,14 +78,14 @@ class ZoomInButton extends Button {
 
     onButtonClick() {
         console.log('zoom in');
-        if(this.scene.data.sceneScale <= 2.0) {
+        if (this.scene.data.sceneScale <= 2.0) {
             this.scene.data.sceneScale += 0.1;
             console.log('scene zoom', this.scene.data.sceneScale);
-            for(const object of this.scene.data.sceneObjects) {
-                object.container.setScale(this.scene.data.sceneScale);  
+            for (const object of this.scene.data.sceneObjects) {
+                object.container.setScale(this.scene.data.sceneScale);
                 let newX = (object.x * this.scene.data.sceneScale) + this.scene.data.sceneOffset.x;
                 let newY = (object.y * this.scene.data.sceneScale) + this.scene.data.sceneOffset.y;
-                object.container.setPosition(newX, newY);                
+                object.container.setPosition(newX, newY);
             }
         }
     }
@@ -96,17 +96,17 @@ class ZoomOutButton extends Button {
 
     onButtonClick() {
         console.log('zoom out');
-        if(this.scene.data.sceneScale >= 0.1) {
+        if (this.scene.data.sceneScale >= 0.1) {
             this.scene.data.sceneScale -= 0.1;
             console.log('scene zoom', this.scene.data.sceneScale);
-            if(!this.scene.data.sceneOffset) {
-                this.scene.data.sceneOffset = { x:0, y:0 };
+            if (!this.scene.data.sceneOffset) {
+                this.scene.data.sceneOffset = {x: 0, y: 0};
             }
-            for(const object of this.scene.data.sceneObjects) {
+            for (const object of this.scene.data.sceneObjects) {
                 object.container.setScale(this.scene.data.sceneScale);
                 let newX = (object.x * this.scene.data.sceneScale) + this.scene.data.sceneOffset.x;
                 let newY = (object.y * this.scene.data.sceneScale) + this.scene.data.sceneOffset.y;
-                object.container.setPosition(newX, newY);                
+                object.container.setPosition(newX, newY);
             }
         }
     }
@@ -126,139 +126,145 @@ class MainScene extends Phaser.Scene {
 
     loadCourseIntoScene(course) {
 
-        console.log('loading course', course);        
-        let courseTitle = new CourseTitle(this, course, 16, 16);
-        this.data.courseTitle = courseTitle;
-        this.data.sceneObjects.push(courseTitle);
+        console.log('loading course', course);
+        this.mainContainer = this.add.container(50,50);
+        
+        
+//        let courseTitle = new CourseTitle(this, course, 16, 16);
+//        this.data.courseTitle = courseTitle;
+//        this.data.sceneObjects.push(courseTitle);
 
-        let buttonPanel = new ButtonPanel(this, 4, 4, 'Aprendiz Dashboard');
-        buttonPanel.addButton(new SaveButton(this, 800, 20, 'Save'));
-        buttonPanel.addSpacer(48);
-        buttonPanel.addButton(new ZoomInButton(this, 1100, 20, '+'));
-        buttonPanel.addButton(new ZoomOutButton(this, 1150, 20, '-'));
-        buttonPanel.addSpacer(96);
-        buttonPanel.addButton(new FullScreenButton(this, 1450, 20, 'Full-Screen'));
+//        let buttonPanel = new ButtonPanel(this, 4, 4, 'Aprendiz Dashboard');
+//        buttonPanel.addButton(new SaveButton(this, 800, 20, 'Save'));
+//        buttonPanel.addSpacer(48);
+//        buttonPanel.addButton(new ZoomInButton(this, 1100, 20, '+'));
+//        buttonPanel.addButton(new ZoomOutButton(this, 1150, 20, '-'));
+//        buttonPanel.addSpacer(96);
+//        buttonPanel.addButton(new FullScreenButton(this, 1450, 20, 'Full-Screen'));
 
-        let ypos = 200;
-        let xpos = 100;
-        for (const courseWork of course.courseWork) {
-            this.data.sceneObjects.push(new CourseWorkRect(this, course, courseWork, xpos, ypos));
-            ypos += 100;
-            xpos += 80;
-        }
+//        let ypos = 200;
+//        let xpos = 100;
+//        for (const courseWork of course.courseWork) {
+//            this.data.sceneObjects.push(new CourseWorkRect(this, course, courseWork, xpos, ypos));
+//            ypos += 100;
+//            xpos += 80;
+//        }
         // mouse down in client area
-        this.input.on('pointerdown', function (event) {
+        this.mainContainer.input.on('pointerdown', function (event) {
+            console.log('pointerdown', event);
             //console.log('pointer down in desktop', event);
-            if(!this.scene.data.buttonClicked) {
-                if(!this.scene.data.dragContext) {
-                    if(!this.scene.data.sceneDragContext) {
-                        this.scene.data.sceneDragContext = {
-                            pointerX: event.downX,
-                            pointerY: event.downY
-                        };
-                        console.log('set scene drag context', this.scene.data.sceneDragContext);
-                    }
-                }
-            }
+//            if (!this.scene.data.buttonClicked) {
+//                if (!this.scene.data.dragContext) {
+//                    if (!this.scene.data.sceneDragContext) {
+//                        this.scene.data.sceneDragContext = {
+//                            pointerX: event.downX,
+//                            pointerY: event.downY
+//                        };
+//                        console.log('set scene drag context', this.scene.data.sceneDragContext);
+//                    }
+//                }
+//            }
         });
-        this.input.on('pointermove', function (event) {
-            let dragContext = this.scene.data.dragContext;
-            if (dragContext) {
-                let x = event.worldX;
-                let y = event.worldY;
-                dragContext.dragRect.setPosition(x + dragContext.deltaX, y + dragContext.deltaY);
-            } else if(this.scene.data.sceneDragContext) {
-                // drag scene
-                if(!this.scene.data.sceneOffset) {
-                    this.scene.data.sceneOffset = { x:0, y:0 };
-                }
-                let pointerX = event.worldX;
-                let pointerY = event.worldY;
-                console.log('drag scene mouse move', pointerX, pointerY);
-                let offsetX = pointerX - this.scene.data.sceneDragContext.pointerX;
-                let offsetY = pointerY - this.scene.data.sceneDragContext.pointerY;
-                if((offsetX !== 0) && (offsetY !== 0)) {
-//                    this.scene.data.sceneOffset.x += offsetX;
-//                    this.scene.data.sceneOffset.y += offsetY;
-                    console.log('scene offset', this.scene.data.sceneOffset);
-                    for(const object of this.scene.data.sceneObjects) {
-                        // update containers
-                        let newX = (object.x * this.scene.data.sceneScale) + offsetX;
-                        let newY = (object.y * this.scene.data.sceneScale) + offsetY;
-                        object.container.setPosition(newX, newY);
-                    }
-                }
-            }
+        this.mainContainer.input.on('pointermove', function (event) {
+            console.log('pointermove', event);
+//            let dragContext = this.scene.data.dragContext;
+//            if (dragContext) {
+//                let x = event.worldX;
+//                let y = event.worldY;
+//                dragContext.dragRect.setPosition(x + dragContext.deltaX, y + dragContext.deltaY);
+//            } else if (this.scene.data.sceneDragContext) {
+//                // drag scene
+//                if (!this.scene.data.sceneOffset) {
+//                    this.scene.data.sceneOffset = {x: 0, y: 0};
+//                }
+//                let pointerX = event.worldX;
+//                let pointerY = event.worldY;
+//                console.log('drag scene mouse move', pointerX, pointerY);
+//                let offsetX = pointerX - this.scene.data.sceneDragContext.pointerX;
+//                let offsetY = pointerY - this.scene.data.sceneDragContext.pointerY;
+//                if ((offsetX !== 0) && (offsetY !== 0)) {
+////                    this.scene.data.sceneOffset.x += offsetX;
+////                    this.scene.data.sceneOffset.y += offsetY;
+//                    console.log('scene offset', this.scene.data.sceneOffset);
+//                    for (const object of this.scene.data.sceneObjects) {
+//                        // update containers
+//                        let newX = (object.x * this.scene.data.sceneScale) + offsetX;
+//                        let newY = (object.y * this.scene.data.sceneScale) + offsetY;
+//                        object.container.setPosition(newX, newY);
+//                    }
+//                }
+//            }
         });
-        this.input.on('pointerup', function (event) {
-            if(this.scene.data.buttonClicked) {
-                delete this.scene.data.buttonClicked;
-            }
-            let x = event.worldX;
-            let y = event.worldY;
-            let dragContext = this.scene.data.dragContext;
-            if (dragContext) {
-                console.log('drag context mouse up', x, y, dragContext);
-                let newX = x + dragContext.deltaX;
-                let newY = y + dragContext.deltaY;
-                dragContext.container.setPosition(newX, newY);
-                dragContext.parentObject.x = newX;
-                dragContext.parentObject.y = newY;
-
-                // update metadata (to save on server if save button is pushed)
-                let metadata;
-                if (dragContext.parentObject.course) {
-                    metadata = dragContext.parentObject.course.metadata;
-                } else {
-                    metadata = dragContext.parentObject.scene.globalMetadata;
-                }
-                if (!metadata) {
-                    metadata = {containerPositions: {}};
-                    dragContext.parentObject.course.metadata = metadata;
-                }
-                let containerId = dragContext.parentObject.containerId;
-                if (dragContext.parentObject.course) {
-                    // course metadata                    
-                    let containerPosition = metadata.containerPositions[containerId];
-                    if (!containerPosition) {
-                        metadata.courseId = dragContext.parentObject.course.id;
-                        metadata.containerPositions[containerId] = {x: newX, y: newY};
-                    } else {
-                        containerPosition.x = newX;
-                        containerPosition.y = newY;
-                    }
-                } else {
-                    // global metadata                    
-                    let containerPosition = metadata.containerPositions[containerId];
-                    if (!containerPosition) {
-                        metadata.containerPositions[containerId] = {x: newX, y: newY};
-                    } else {
-                        containerPosition.x = newX;
-                        containerPosition.y = newY;
-                    }
-                }
-                console.log('update course configuration', metadata);
-                dragContext.dragRect.destroy();
-                // update configuration
-                delete this.scene.data.dragContext;
-            } else if(this.scene.data.sceneDragContext) {
-                //this.scene.data.courseTitle.container.setPosition(10,10);
-                console.log('pointer up scene drag', this.scene.data.sceneDragContext);
-                let pointerX = event.upX;
-                let pointerY = event.upY;
-                let offsetX = pointerX - this.scene.data.sceneDragContext.pointerX;
-                let offsetY = pointerY - this.scene.data.sceneDragContext.pointerY;
-                if((offsetX !== 0) && (offsetY !== 0)) {
-                    delete this.scene.data.sceneDragContext;
-                    console.log('drag scene', offsetX, offsetY);
-                    if(!this.scene.data.sceneOffset) {
-                        this.scene.data.sceneOffset = { x:0, y:0 };
-                    }
-                    this.scene.data.sceneOffset.x = offsetX;
-                    this.scene.data.sceneOffset.y = offsetY;
-                    console.log('scene offset', this.scene.data.sceneOffset);
-                }
-            }
+        this.mainContainer.input.on('pointerup', function (event) {
+            console.log('pointerup', event);
+//            if (this.scene.data.buttonClicked) {
+//                delete this.scene.data.buttonClicked;
+//            }
+//            let x = event.worldX;
+//            let y = event.worldY;
+//            let dragContext = this.scene.data.dragContext;
+//            if (dragContext) {
+//                console.log('drag context mouse up', x, y, dragContext);
+//                let newX = x + dragContext.deltaX;
+//                let newY = y + dragContext.deltaY;
+//                dragContext.container.setPosition(newX, newY);
+//                dragContext.parentObject.x = newX;
+//                dragContext.parentObject.y = newY;
+//
+//                // update metadata (to save on server if save button is pushed)
+//                let metadata;
+//                if (dragContext.parentObject.course) {
+//                    metadata = dragContext.parentObject.course.metadata;
+//                } else {
+//                    metadata = dragContext.parentObject.scene.globalMetadata;
+//                }
+//                if (!metadata) {
+//                    metadata = {containerPositions: {}};
+//                    dragContext.parentObject.course.metadata = metadata;
+//                }
+//                let containerId = dragContext.parentObject.containerId;
+//                if (dragContext.parentObject.course) {
+//                    // course metadata                    
+//                    let containerPosition = metadata.containerPositions[containerId];
+//                    if (!containerPosition) {
+//                        metadata.courseId = dragContext.parentObject.course.id;
+//                        metadata.containerPositions[containerId] = {x: newX, y: newY};
+//                    } else {
+//                        containerPosition.x = newX;
+//                        containerPosition.y = newY;
+//                    }
+//                } else {
+//                    // global metadata                    
+//                    let containerPosition = metadata.containerPositions[containerId];
+//                    if (!containerPosition) {
+//                        metadata.containerPositions[containerId] = {x: newX, y: newY};
+//                    } else {
+//                        containerPosition.x = newX;
+//                        containerPosition.y = newY;
+//                    }
+//                }
+//                console.log('update course configuration', metadata);
+//                dragContext.dragRect.destroy();
+//                // update configuration
+//                delete this.scene.data.dragContext;
+//            } else if (this.scene.data.sceneDragContext) {
+//                //this.scene.data.courseTitle.container.setPosition(10,10);
+//                console.log('pointer up scene drag', this.scene.data.sceneDragContext);
+//                let pointerX = event.upX;
+//                let pointerY = event.upY;
+//                let offsetX = pointerX - this.scene.data.sceneDragContext.pointerX;
+//                let offsetY = pointerY - this.scene.data.sceneDragContext.pointerY;
+//                if ((offsetX !== 0) && (offsetY !== 0)) {
+//                    delete this.scene.data.sceneDragContext;
+//                    console.log('drag scene', offsetX, offsetY);
+//                    if (!this.scene.data.sceneOffset) {
+//                        this.scene.data.sceneOffset = {x: 0, y: 0};
+//                    }
+//                    this.scene.data.sceneOffset.x = offsetX;
+//                    this.scene.data.sceneOffset.y = offsetY;
+//                    console.log('scene offset', this.scene.data.sceneOffset);
+//                }
+//            }
         });
     }
 
@@ -267,18 +273,20 @@ class MainScene extends Phaser.Scene {
         this.globalMetadata = config.global;
         this.courses = config.courses;
         if (!this.globalMetadata) {
-            this.globalMetadata = {containerPositions: {}};
+            this.globalMetadata = {
+                containerPositions: {}
+            };
         }
         // open websocket
         this.websocket = new WebSocketContext(this.sys.game.scene, this.onWebSocketOpen.bind(this));
         this.data.sceneObjects = [];
         this.data.sceneScale = 1.0;
         // now we have access to courses
-        for (const course of this.courses) {
-            this.loadCourseIntoScene(course);
-        }
-        // legend rect
-        this.data.sceneObjects.push(new LegendRect(this, this.courses, 16, 64));
+//        for (const course of this.courses) {
+//            this.loadCourseIntoScene(course);
+//        }
+//        // legend rect
+//        this.data.sceneObjects.push(new LegendRect(this, this.courses, 16, 64));
     }
 }
 ;
@@ -288,7 +296,7 @@ angular.module("AprendizApplication", ['ngCookies']);
 require('./ClassroomDataLoaderService.js');
 
 angular.module("AprendizApplication").controller('MainViewController', function (ClassroomDataLoaderService) {
-    
+
     console.log('started main view controller');
 
     let screenWidth = window.screen.width;
