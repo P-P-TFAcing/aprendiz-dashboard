ade1e57650a8522499b09a4285a4cdff6a051b35
Timothy Heider <timothy.heider@gmail.com>
2022-09-12 08:46:06 -0700

    scroll

--- src/main/js/ScrollableContainer.js
+++ src/main/js/ScrollableContainer.js
@@ -1,66 +1,47 @@
-import PointerEventHandler from './PointerEventHandler.js';
 
-class RegionPointerEventHandler extends PointerEventHandler {
+export default class ScrollableContainer extends Phaser.GameObjects.Container {
 
-    constructor(scene, parentScrollableContainer) {
-        super(scene);
-        this.parentScrollableContainer = parentScrollableContainer;
+    constructor(scene, x, y) {
+        super(scene, x, y);
+        scene.input.on('pointermove', this.onPointerMove.bind(this));
+        scene.input.on('pointerdown', this.onPointerDown.bind(this));
+        scene.input.on('pointerup', this.onPointerUp.bind(this));        
     }
-
-    onPointerDown(event, scene) {
+    
+    onPointerDown(event) {
         let parent = this.parentScrollableContainer;
         let x = event.downX;
         let y = event.downY;
         if ((x >= parent.x) && (y >= parent.y)) {
             // mark the global start
             this.dragContext = {
-                position: {x: x, y: y}
+                containerStartPosition: {
+                    x: parent.x,
+                    y: parent.y
+                }
             };
             console.log('start dragging', this.dragContext);
         }
     }
 
-    onPointerUp(event, scene) {
-        delete this.dragContext;
-    }
-
-    onPointerMove(event, scene) {
-        if (this.dragContext) {
-            let parent = this.parentScrollableContainer;
-            let x = event.position.x;
-            let y = event.position.y;
-            if ((x >= parent.x) && (y >= parent.y)) {
-                let deltaX = x - this.dragContext.position.x;
-                let deltaY = y - this.dragContext.position.y;
-                console.log('drag scene', event.position, deltaX, deltaY);
-                // scroll the scene
-                parent.scrollRegion(deltaX, deltaY);
-                this.dragContext.position.x += deltaX;
-                this.dragContext.position.y += deltaY;
-            }
+    onPointerUp(event) {
+        if(this.dragContext) {
+            delete this.dragContext;
         }
     }
 
-}
-
-export default class ScrollableContainer extends Phaser.GameObjects.Container {
-
-    constructor(scene, x, y) {
-        super(scene, x, y);
-        this.sceneOffset = {x: 0, y: 0};
-        new RegionPointerEventHandler(scene, this);
-    }
-
-    translateObject(gameObject, deltaX, deltaY) {        
-        gameObject.setPosition(gameObject.x + deltaX, gameObject.y + deltaY)
-    }
-
-    scrollRegion(deltaX, deltaY) {
-        console.log('scrollRegion', deltaX, deltaY);
-        this.sceneOffset.x += deltaX;
-        this.sceneOffset.x += deltaY;
-        this.iterate(this.translateObject, this, deltaX, deltaY);
-    }
+    onPointerMove(event) {
+        if (this.dragContext) {
+            let x = event.position.x;
+            let y = event.position.y;
+            if ((x >= this.x) && (y >= this.y)) {
+                let deltaX = x - this.dragContext.containerStartPosition.x;
+                let deltaY = y - this.dragContext.containerStartPosition.y;
+                console.log('drag scene', event.position, deltaX, deltaY);
+                this.setPosition(this.dragContext.containerStartPosition.x + deltaX, this.dragContext.containerStartPosition.y + deltaY);
+            }
+        }
+    }    
 
 }
 
