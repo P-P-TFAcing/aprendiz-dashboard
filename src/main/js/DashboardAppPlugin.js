540c3e3f4bafd50c7f53a913fdee7340e816f22f
Timothy Heider <timothy.heider@gmail.com>
2022-09-12 09:49:49 -0700

    course title as object

--- src/main/js/DashboardAppPlugin.js
+++ src/main/js/DashboardAppPlugin.js
@@ -4,11 +4,11 @@ import CourseTitle from './CourseTitle.js';
 export default class DashboardAppPlugin extends Phaser.Plugins.BasePlugin {
 
     constructor(pluginManager) {
-        super(pluginManager);        
-        pluginManager.registerGameObject('scrollableContainer', function(x,y, width, height) {
+        super(pluginManager);
+        pluginManager.registerGameObject('scrollableContainer', function (x, y, width, height) {
             return this.displayList.add(new ScrollableContainer(this.scene, x, y, width, height));
         });
-        pluginManager.registerGameObject('courseTitle', function(x,y, course) {
+        pluginManager.registerGameObject('courseTitle', function (x, y, course) {
             return this.displayList.add(new CourseTitle(this.scene, x, y, course));
         });
     }
