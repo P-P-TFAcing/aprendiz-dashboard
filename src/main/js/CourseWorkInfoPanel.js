8f3f7d4cf1890739dfbd4ceef88245d2319c8d2f
Timothy Heider <timothy.heider@gmail.com>
2022-11-10 17:57:38 -0800

    course info panel

--- src/main/js/CourseWorkInfoPanel.js
+++ src/main/js/CourseWorkInfoPanel.js
@@ -5,11 +5,12 @@ export default class CourseWorkInfoPanel {
         this.container.setVisible(visible);
     }
         
-    constructor(scene, courses, x, y) {        
-        this.courses = courses;
+    constructor(scene, scrollableContainer, x, y, course, courseWork) {
+        this.course = course;
+        this.courseWork = courseWork;
         this.scene = scene;
         this.container = scene.add.container(x, y);
-        let infoPanelRect = scene.add.rectangle(0, 0, 400, 400);
+        let infoPanelRect = scene.add.rectangle(20, 20, 400, 400);
         infoPanelRect.setOrigin(0, 0);
         infoPanelRect.setStrokeStyle(2, 0x00ff00, 2);
         infoPanelRect.setFillStyle(0x800000, 0.6);
