da08ebe8652d7cea68662ee5af1bba60de78b4db
Timothy Heider <timothy.heider@gmail.com>
2022-12-05 16:44:06 -0800

    color picker

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -100,7 +100,7 @@ class CourseColorButton extends Button {
         console.log('course color');    
         var elemDiv = document.createElement('div');
         elemDiv.style.cssText = 'position:absolute;width:300px;height:120px;opacity:1.0;z-index:1;background:#fff;top:150px;left:150px;color:#000';
-        let htmlText = '<div><div><span><input type="color" value="#ff0000"> Course Color</span></div><div><span><button>Update</button><button>Close</button></span></div></div>';
+        let htmlText = '<div style="padding: 32px;"><div><span><input type="color" value="#ff0000"> Course Color</span></div><div><span><button>Update</button><button>Close</button></span></div></div>';
         elemDiv.innerHTML = htmlText;
         document.body.appendChild(elemDiv);
     }
